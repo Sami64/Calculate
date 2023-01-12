7b9d509020d3b1b6af003d7249a9e56bab05eb42
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-01-12 22:11:57 +0000

    start ui elements

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -1,13 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using NCalc;
 using TMPro;
+using UnityEditor.Animations;
 
 public class Calculator : MonoBehaviour
 {
     // Display Panel Variables
     [SerializeField] private TextMeshProUGUI displayText;
+    [SerializeField] private GameObject limitText;
+    [SerializeField] private TextMeshProUGUI previewText;
 
-    
+    public LeanTweenType tweenType;
+    private bool isTweenedIn = true;
+    private bool tweenComplete = true;
 
     string Calculate(string equation)
     {
@@ -25,12 +31,31 @@ public class Calculator : MonoBehaviour
     public void EvaluateExpression()
     {
         string answer = Calculate(displayText.text);
-        displayText.text = answer;
+        previewText.text = answer;
+        
+        //previewText.gameObject.GetComponent<Animator>().Play("PreviewAnimation");
+        
+        displayText.text = "";
     }
 
     public void UpdateDisplay(string buttonValue)
     {
-        displayText.text += buttonValue;
+        if (displayText.text.Length <= 14)
+        {
+            displayText.text += buttonValue; 
+        }
+        else
+        {
+            StartCoroutine(ShowLimitText());
+        }
+        
+    }
+
+    IEnumerator ShowLimitText()
+    {
+        limitText.SetActive(true);
+        yield return new WaitForSeconds(2);
+        limitText.SetActive(false);
     }
 
     #endregion
